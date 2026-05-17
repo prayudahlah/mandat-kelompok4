d3472fa6122deb3dd5cab74447fc82446b3eca45
Dien <akbardian64@gmail.com>
2026-05-17 11:00:02 +0700

    feat: add users, products, date table dimension

--- scripts/warehouse.sql
+++ scripts/warehouse.sql
@@ -1,3 +1,29 @@
+CREATE TABLE dim_user (
+    user_id INT PRIMARY KEY,
+    province_id INT NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    farm_name VARCHAR(255),
+    status VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE dim_product (
+    product_id INT PRIMARY KEY,
+    category_id INT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    is_active INT NOT NULL  
+);
+
+CREATE TABLE dim_date (
+    date_id INT PRIMARY KEY,
+    full_date DATE NOT NULL,
+    year INT NOT NULL,
+    quarter INT NOT NULL,
+    month_name VARCHAR(20) NOT NULL,
+    day_of_week INT NOT NULL,
+    is_holiday BOOLEAN NOT NULL,
+);
+
 CREATE TABLE fact_negotiations (
     negotiations_id INT IDENTITY(1,1) PRIMARY KEY,
     start_date_id INT NOT NULL,
@@ -11,21 +37,33 @@ CREATE TABLE fact_negotiations (
     price_difference DECIMAL(18,2),
     agreed_quantity_kg DECIMAL(18,2),
     negotiations_duration_hours DECIMAL(10,2),
-    total_chat_turns INT
+    total_chat_turns INT,
+    
+    CONSTRAINT FK_fact_negotiations_start_date FOREIGN KEY (start_date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_negotiations_end_date FOREIGN KEY (end_date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_negotiations_buyer FOREIGN KEY (buyer_id) REFERENCES dim_user(user_id),
+    CONSTRAINT FK_fact_negotiations_seller FOREIGN KEY (seller_id) REFERENCES dim_user(user_id),
+    CONSTRAINT FK_fact_negotiations_product FOREIGN KEY (product_id) REFERENCES dim_product(product_id)
 );
 
 CREATE TABLE fact_sales (
     sales_id INT IDENTITY(1,1) PRIMARY KEY,
     date_id INT NOT NULL,
     buyer_id INT NOT NULL,
+    seller_id INT NOT NULL,
     destination_province_id INT NOT NULL,
     origin_province_id INT NOT NULL,
-    seller_id INT NOT NULL,
     product_id INT NOT NULL,
     order_status_id INT NOT NULL,
     quantity_kg DECIMAL(18,2),
     unit_price DECIMAL(18,2),
     discount DECIMAL(18,2),
     total_amount DECIMAL(18,2),
-    is_interprovince INT
+    is_interprovince INT,  
+    
+    CONSTRAINT FK_fact_sales_date FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_sales_buyer FOREIGN KEY (buyer_id) REFERENCES dim_user(user_id),
+    CONSTRAINT FK_fact_sales_seller FOREIGN KEY (seller_id) REFERENCES dim_user(user_id),
+    CONSTRAINT FK_fact_sales_product FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
 );
+
