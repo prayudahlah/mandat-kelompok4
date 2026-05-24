453f4ebfafe92eed7cca0f8d31acbf4602cfbcdb
Dien <akbardian64@gmail.com>
2026-05-24 14:42:36 +0700

    feat: add new dim and fact table

--- scripts/ddl/dw/01_data_warehouse.sql
+++ scripts/ddl/dw/01_data_warehouse.sql
@@ -26,7 +26,7 @@ CREATE TABLE dim_product_categories (
 
 CREATE TABLE dim_products (
     product_key INT PRIMARY KEY IDENTITY(1,1),
-    category_key INT NOT NULL,  -- Ganti dari category_id
+    category_key INT NOT NULL,  
     product_id INT NOT NULL,
     product_name VARCHAR(255) NOT NULL,
     is_active INT NOT NULL,
@@ -76,8 +76,13 @@ CREATE TABLE fact_negotiations (
     CONSTRAINT FK_fact_negotiations_product FOREIGN KEY (product_key) REFERENCES dim_products(product_key)
 );
 
-CREATE TABLE fact_sales (
-    sales_id INT PRIMARY KEY IDENTITY(1,1),
+CREATE TABLE dim_contract_status (
+    contract_status_id INT PRIMARY KEY,
+    status_code VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE fact_order_items (
+    order_item_id BIGINT PRIMARY KEY,           
     date_id INT NOT NULL,
     buyer_key INT NOT NULL,
     seller_key INT NOT NULL,
@@ -91,11 +96,45 @@ CREATE TABLE fact_sales (
     total_amount DECIMAL(18,2),
     is_interprovince INT,
 
-    CONSTRAINT FK_fact_sales_date FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
-    CONSTRAINT FK_fact_sales_buyer FOREIGN KEY (buyer_key) REFERENCES dim_users(user_key),
-    CONSTRAINT FK_fact_sales_seller FOREIGN KEY (seller_key) REFERENCES dim_users(user_key),
-    CONSTRAINT FK_fact_sales_destination_province FOREIGN KEY (destination_province_key) REFERENCES dim_province(province_key),
-    CONSTRAINT FK_fact_sales_origin_province FOREIGN KEY (origin_province_key) REFERENCES dim_province(province_key),
-    CONSTRAINT FK_fact_sales_product FOREIGN KEY (product_key) REFERENCES dim_products(product_key),
-    CONSTRAINT FK_fact_sales_order_status FOREIGN KEY (order_status_id) REFERENCES dim_order_status(order_status_id)
+    CONSTRAINT FK_fact_order_items_date 
+        FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_order_items_buyer 
+        FOREIGN KEY (buyer_key) REFERENCES dim_users(user_key),
+    CONSTRAINT FK_fact_order_items_seller 
+        FOREIGN KEY (seller_key) REFERENCES dim_users(user_key),
+    CONSTRAINT FK_fact_order_items_dest_province 
+        FOREIGN KEY (destination_province_key) REFERENCES dim_province(province_key),
+    CONSTRAINT FK_fact_order_items_origin_province 
+        FOREIGN KEY (origin_province_key) REFERENCES dim_province(province_key),
+    CONSTRAINT FK_fact_order_items_product 
+        FOREIGN KEY (product_key) REFERENCES dim_products(product_key),
+    CONSTRAINT FK_fact_order_items_order_status 
+        FOREIGN KEY (order_status_id) REFERENCES dim_order_status(order_status_id)
+);
+
+CREATE TABLE fact_contracts (
+    contract_id BIGINT PRIMARY KEY,             
+    start_date_id INT NOT NULL,
+    end_date_id INT NOT NULL,
+    buyer_key INT NOT NULL,
+    seller_key INT NOT NULL,
+    destination_province_key INT NOT NULL,
+    origin_province_key INT NOT NULL,
+    total_amount DECIMAL(18,2),
+    contract_status_id INT NOT NULL,            
+
+    CONSTRAINT FK_fact_contracts_start_date 
+        FOREIGN KEY (start_date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_contracts_end_date 
+        FOREIGN KEY (end_date_id) REFERENCES dim_date(date_id),
+    CONSTRAINT FK_fact_contracts_buyer 
+        FOREIGN KEY (buyer_key) REFERENCES dim_users(user_key),
+    CONSTRAINT FK_fact_contracts_seller 
+        FOREIGN KEY (seller_key) REFERENCES dim_users(user_key),
+    CONSTRAINT FK_fact_contracts_dest_province 
+        FOREIGN KEY (destination_province_key) REFERENCES dim_province(province_key),
+    CONSTRAINT FK_fact_contracts_origin_province 
+        FOREIGN KEY (origin_province_key) REFERENCES dim_province(province_key),
+    CONSTRAINT FK_fact_contracts_status 
+        FOREIGN KEY (contract_status_id) REFERENCES dim_contract_status(contract_status_id)
 );
